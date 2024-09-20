bebd384d8e345c1973a95b1406a689366c78867f
dulivu <GeorgeL1102@gmail.com>
2024-09-20 18:54:30 -0500

    [UPD] actualizacion de modulo

---
+++ AdapterTwig/Controller.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace IrbisApps\AdapterTwig;
+
+use Irbis\Controller as iController;
+use Irbis\Server;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+
+/**
+ * este modulo requiere twig instalado por composer
+ * composer require "twig/twig:^3.0"
+ * 
+ * order: debe ser el primer módulo en ser llamado
+ */
+class Controller extends iController {
+    public $name 			= 'twig';
+	public $has_routes      = false;
+	public $installable 	= false;
+	public $depends 		= [];
+
+    public $loader;
+    public $environment;
+
+    public function __construct () {
+        parent::__construct();
+
+        $server = Server::getInstance();
+        # NOTE: se puede usar esta condición para evitar que se
+        # vuelva a ejecutar un bloque de código al crear el objeto
+        if (!$server->getController('IrbisApps/AdapterTwig')) {
+            $loader = $this->loader = new FilesystemLoader(BASE_PATH);
+            $environment = $this->environment = new Environment($loader, ['debug' => DEBUG_MODE]);
+            $environment->addGlobal('DEBUG_MODE', DEBUG_MODE ? 1 : 0);
+            // agregamos la extensión para debug
+            $environment->addExtension(new DebugExtension());
+            // establecemos la función de renderización
+            $server->render = function ($view, $data) use ($environment) {
+                die($environment->render($view, $data));
+            };
+            // por cada controlador agregado registrar en twig una ruta
+            // así se puede usar rutas de vista e: @twig/views/index.html
+            $server->on('addController', function ($controller, $name) use ($loader) {
+                if (!$name) throw new \Exception("Twig: el controlador '{$controller->namespace}' requiere un nombre");
+                if ($controller->views) $loader->addPath($controller->file("/{$controller->views}"), $name);
+            });
+            // cambiar las vistas de error por plantilla twig
+            $server->view_404 = 'IrbisApps/AdapterTwig/views/404.html';
+            $server->view_500 = 'IrbisApps/AdapterTwig/views/500.html';
+        }
+    }
+}
